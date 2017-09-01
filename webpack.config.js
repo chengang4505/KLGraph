5745d7ff6ccd5d944ce1d053c6545ba863f985a6
chengang <chengang@bbdservice.com>
2017-09-01 17:07:47 +0800

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
             },
             {
                 test: /\.glsl/,
-                use: 'raw-loader'
+                loader: 'raw-loader'
             }
         ]
     }
